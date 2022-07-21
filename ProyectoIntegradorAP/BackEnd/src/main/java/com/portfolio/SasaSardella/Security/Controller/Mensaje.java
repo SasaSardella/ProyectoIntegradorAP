75a289f8906a77cb41c1e96da9d5e03966d51207
Sabrina Sardella <sardellasabrina@gmail.com>
2022-07-21 11:14:25 -0300

    Creacion Mensaje

---
+++ ProyectoIntegradorAP/BackEnd/src/main/java/com/portfolio/SasaSardella/Security/Controller/Mensaje.java
@@ -0,0 +1,29 @@
+
+package com.portfolio.SasaSardella.Security.Controller;
+
+
+public class Mensaje {
+    private String mensaje;
+    
+    //Contructores
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+    //Getter y Setter
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+}
