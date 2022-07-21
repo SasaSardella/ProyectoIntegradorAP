fba7f668e638934e6f56fac2d7c2640c4e131805
Sabrina Sardella <sardellasabrina@gmail.com>
2022-07-21 11:16:05 -0300

    Creacion iUsuarioRepository

---
+++ ProyectoIntegradorAP/BackEnd/src/main/java/com/portfolio/SasaSardella/Security/Repository/iUsuarioRepository.java
@@ -0,0 +1,15 @@
+
+package com.portfolio.SasaSardella.Security.Repository;
+
+import com.portfolio.SasaSardella.Security.Entity.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository 
+public interface iUsuarioRepository extends JpaRepository<Usuario, Integer>{
+    Optional<Usuario> findByNonbreUsuario(String nombreUsuario);
+    
+    boolean existsByNombreUsuario(String nombreUsuario);
+    boolean existsByEmail(String email);
+}
